257e1a6d6deffdc058ea2ba44490622b7e23b63c
Evan Mullins <evanrm@gmail.com>
2021-01-05 10:55:27 -0500

    song archive layout

--- archive-song.php
+++ archive-song.php
@@ -13,8 +13,34 @@ get_header(); ?>
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php get_template_part( 'song', 'list' ); ?>
+		<h1>All Songs</h1>
+		<?php get_template_part( 'song', 'list', array(
+			'index' =>   true,
+			'show_artists' => true,
+		) ); ?>
+<?php /*
+		<h1>By Artist</h1>
+		<ul class="song-list">
+		<?php 
+		$artists = get_terms( array(
+			'taxonomy' => 'artist',
+			'orderby' => 'name',
+            'order' => 'ASC',
+			'hide_empty' => true,
+		) );
+		if( !empty($artists)) {
+			foreach( $artists as $artist ) {
+				?>
+				<li><h1 class="alpha_index"><?php echo $artist->name; ?></h1></li>
 
+				<?php get_template_part( 'song', 'list', array(
+					'artist' => $artist->slug,
+				) ); ?>
+				<?php
+			}
+		}
+		?>
+*/ ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
--- song-list.php
+++ song-list.php
@@ -1,23 +1,72 @@
 <?php
-$args = array (
-	'post_type'             => 'song',
-	'posts_per_page'        => -1,
-	'pagination'            => false,
-	'orderby' 				=> 'title', 
-	'order'					=> 'ASC'
+
+// Set defaults.
+$args = wp_parse_args(
+	$args,
+	array(
+		'index'        => false,
+		'show_artists' => false,
+		'artist'       => false, 
+	)
+);
+
+if ( $args['artist'] ) {
+	$tax_query = array(
+		array(
+			'taxonomy' => 'artist',
+			'field'    => 'slug',
+			'terms'    => $args['artist']
+		)
+	);
+}
+
+$song_query_args = array (
+	'post_type'      => 'song',
+	'posts_per_page' => -1,
+	'pagination'     => false,
+	'orderby'        => 'title', 
+	'order'          => 'ASC',
+	'tax_query'      => $args['artist'] ? $tax_query : false,
 );
 
 // The Query
-$query = new WP_Query( $args );
+$song_query = new WP_Query( $song_query_args );
 
 // The Loop
-if ( $query->have_posts() ) { 
+if ( $song_query->have_posts() ) { 
 	echo '<ul class="song-list">';
 	$count = 0;
-	while ( $query->have_posts() ) {
-		$query->the_post();
+	$alpha_count = '';
+
+	while ( $song_query->have_posts() ) {
+		$song_query->the_post();
+
+		$title = get_the_title();
+		$alpha_title = $title[0];
+
+		if ( $args['index'] && $alpha_count !== $alpha_title ) {
+			$alpha_count = $alpha_title;
+			echo '<li class="alpha_count_'.$alpha_count.'"><h1 class="alpha_index">' . $alpha_count . '</h1></li>';
+		}
+
+		if ( $args['show_artists'] ) {
+			$artist_list = '';
+			// include artist
+			$artist_object_list = get_the_terms( $post->ID, 'artist' );
+			if ( ! empty( $artist_object_list ) ) {
+				$artist_list = join(', ', wp_list_pluck($artist_object_list, 'name'));
+			}
+		}
+
 		?>	
-			<li class="count_<?php echo $count++; ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+			<li class="count_<?php echo $count++; ?>">
+				<a 
+					href="<?php the_permalink(); ?>"
+				>
+					<?php the_title(); ?>
+					<?php echo $args['show_artists'] && !empty( $artist_list ) ? ' - ' . $artist_list : ''; ?>
+				</a>
+			</li>
 		<?php
 	} //endwhile
 	echo '</ul>';
